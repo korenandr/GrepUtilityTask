fc5dc8a4b8d9669517b038e6c7cb8f0421e97bec
andrei <and.korenevich@yandex.by>
2024-10-24 16:57:41 +0300

    Remove build directory from vcs

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrei/mapbox/test
-# Build directory: /home/andrei/mapbox/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(GrepUtilityTest "/home/andrei/mapbox/build/test/GrepUtilityTest")
-set_tests_properties(GrepUtilityTest PROPERTIES  _BACKTRACE_TRIPLES "/home/andrei/mapbox/test/CMakeLists.txt;18;add_test;/home/andrei/mapbox/test/CMakeLists.txt;0;")
